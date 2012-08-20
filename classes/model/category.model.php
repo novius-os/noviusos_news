bf4c5967864cee14098a3bbcb6f8dc7a20db9222
Gilles FELIX <felix@novius.com>
2012-08-20 11:52:44 +0200

    Merge dev

--- classes/model/category.model.php
+++ classes/model/category.model.php
@@ -7,7 +7,6 @@ class Model_Category extends \Nos\BlogNews\Model_Category
     protected static $_table_name = 'nos_news_category';
 
     public static function _init() {
-        parent::_init();
         static::$_behaviours['Nos\Orm_Behaviour_Url']['urls'][] = array('Nos\BlogNews\News\Controller_Front', 'url_model');
     }
 
