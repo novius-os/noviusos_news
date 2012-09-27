cd21d6f56afdf91b926aae6b8792365eac1efcc7
Gilles FELIX <felix@novius.com>
2012-09-27 17:09:55 +0200

    $COMMITMES

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -45,16 +45,6 @@ return array(
                 'height' => 400,
                 'ajax' => true,
             ),
-            'data_catchers_added' => array(
-                'posts_rss_channel' => array(
-                    'data_catcher' => 'rss_channel',
-                    'title' => __('RSS News stories channel'),
-                ),
-                'comments_rss_channel' => array(
-                    'data_catcher' => 'rss_channel',
-                    'title' => __('RSS Comments channel'),
-                ),
-            ),
         ),
     )
 );
