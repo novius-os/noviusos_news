5f8b1fb3da04e789366d2bbd535cc473ae0788cc
Julian Esperat <esperat@novius.fr>
2013-01-30 15:49:01 +0100

    "Updated translations"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,9 +1,9 @@
 <?php
 
-// Generated on 23/01/2013 11:25:53
+// Generated on 29/01/2013 19:08:29
 
-// 24 out of 24 messages are translated (100%).
-// 112 out of 112 words are translated (100%).
+// 22 out of 22 messages are translated (100%).
+// 106 out of 106 words are translated (100%).
 
 return array(
     #: classes/model/post.model.php:14
@@ -11,10 +11,6 @@ return array(
 
     #: classes/model/post.model.php:15
     'Use news story summary' => 'Reprendre le chapeau de l’actualité',
-
-    #: config/controller/admin/post.config.php:18
-    #: config/common/post.config.php:16
-    'Add a news story' => 'Ajouter une actualité',
 
     #: config/controller/admin/appdesk.config.php:15
     #: config/controller/admin/appdesk.config.php:21
@@ -33,48 +29,55 @@ return array(
     #: config/controller/admin/appdesk.config.php:24
     'No news stories' => 'Pas d’actualité',
 
-    #: config/controller/admin/appdesk.config.php:25
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:26
     'Show all news stories' => 'Afficher toutes les actualités',
 
     #: config/config.php:12
     'News stories' => 'Actualités',
 
+    #: config/common/post.config.php:16
+    'Add a news story' => 'Ajouter une actualité',
+
     #. Crud
-    #: config/common/post.config.php:28
+    #: config/common/post.config.php:21
     'Good! A new story added to your collection.' => 'C’est dans la boite. Actualité ajoutée.',
 
-    #: config/common/post.config.php:29
+    #: config/common/post.config.php:22
     'The news story has been deleted.' => 'L’actualité a été supprimée.',
 
     #. General errors
-    #: config/common/post.config.php:32
+    #: config/common/post.config.php:25
     'This news story doesn’t exist any more. It has been deleted.' => 'Cette actualité n’existe plus. Elle a été supprimée.',
 
-    #: config/common/post.config.php:33
+    #: config/common/post.config.php:26
     'We cannot find this news story.' => 'Nous n’arrivons pas à trouver cette actualité.',
 
     #. Deletion popup
-    #: config/common/post.config.php:40
+    #: config/common/post.config.php:29
     'Deleting the news story ‘{{title}}’' => 'Supprimer l’actualité «&nbsp;{{title}}&nbsp;»',
 
     #. Delete action's labels
-    #: config/common/post.config.php:43
+    #: config/common/post.config.php:32
     'Yes, delete this news story' => 'Oui, supprimer cette actualité',
 
-    #: config/common/post.config.php:45
+    #: config/common/post.config.php:33
+    'Yes, delete these {{count}} news stories' => 'Oui, supprimer ces {{count}} actualités',
+
+    #: config/common/post.config.php:35
     '1 news story' => '1 actualité',
 
-    #: config/common/post.config.php:46
+    #: config/common/post.config.php:36
     '{{count}} news stories' => '{{count}} actualités',
 
     #. Keep only if the model has the behaviour Contextable
-    #: config/common/post.config.php:49
+    #: config/common/post.config.php:39
     'This news story exists in <strong>{{context_count}} contexts</strong>.' => 'Cette actualité existe dans <strong>{{context_count}} contextes</strong>.',
 
-    #: config/common/post.config.php:50
+    #: config/common/post.config.php:40
     'This news story exists in <strong>{{language_count}} languages</strong>.' => 'Cette actualité existe dans <strong>{{language_count}} langues</strong>.',
 
-    #: config/common/category.config.php:14
+    #: config/common/category.config.php:15
     'Add a category' => 'Ajouter une catégorie',
 
 );
