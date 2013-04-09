023b0a19415e32d8ff55e916d3e99016598ffdb9
Julian Esperat <esperat@novius.fr>
2013-04-09 16:37:43 +0200

    "Adding Japanese\!"

---
+++ lang/ja/common.lang.php
@@ -0,0 +1,83 @@
+<?php
+
+// Generated on 09/04/2013 16:13:10
+
+// 22 out of 22 messages are translated (100%).
+// 106 out of 106 words are translated (100%).
+
+return array(
+    #: classes/model/post.model.php:14
+    'Use news story title' => 'ニュース記事のタイトルを使用する',
+
+    #: classes/model/post.model.php:15
+    'Use news story summary' => 'ニュース記事の概要を使用する',
+
+    #: config/controller/admin/appdesk.config.php:15
+    #: config/controller/admin/appdesk.config.php:21
+    'news stories' => 'ニュース記事',
+
+    #: config/controller/admin/appdesk.config.php:16
+    #: config/controller/admin/appdesk.config.php:20
+    'news story' => 'ニュース記事',
+
+    #: config/controller/admin/appdesk.config.php:22
+    'Showing {{x}} news stories out of {{y}}' => '{{y}}件のニュース記事のうち、{{x}}件を表示しています',
+
+    #: config/controller/admin/appdesk.config.php:23
+    'Showing 1 news story' => '1件のニュース記事を表示しています',
+
+    #: config/controller/admin/appdesk.config.php:24
+    'No news stories' => 'ニュース記事はありません。',
+
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:26
+    'Show all news stories' => 'ニュース記事を全て表示する',
+
+    #: config/config.php:12
+    'News stories' => 'ニュース記事',
+
+    #: config/common/post.config.php:16
+    'Add a news story' => 'ニュース記事を追加',
+
+    #. Crud
+    #: config/common/post.config.php:21
+    'Good! A new story added to your collection.' => 'ニュース記事がアプリケーションに追加されました。',
+
+    #: config/common/post.config.php:22
+    'The news story has been deleted.' => 'ニュース記事が削除されました。',
+
+    #. General errors
+    #: config/common/post.config.php:25
+    'This news story doesn’t exist any more. It has been deleted.' => 'このニュース記事は存在しません。削除されました。',
+
+    #: config/common/post.config.php:26
+    'We cannot find this news story.' => 'このニュース記事は見つかりませんでした。',
+
+    #. Deletion popup
+    #: config/common/post.config.php:29
+    'Deleting the news story ‘{{title}}’' => 'ニュース記事‘{{title}}’を削除しています',
+
+    #. Delete action's labels
+    #: config/common/post.config.php:32
+    'Yes, delete this news story' => 'はい、このニュース記事を削除します',
+
+    #: config/common/post.config.php:33
+    'Yes, delete these {{count}} news stories' => 'はい、これら{{count}}件のニュース記事を削除します',
+
+    #: config/common/post.config.php:35
+    '1 news story' => '1件のニュース記事',
+
+    #: config/common/post.config.php:36
+    '{{count}} news stories' => '{{count}}件のニュース記事',
+
+    #. Keep only if the model has the behaviour Contextable
+    #: config/common/post.config.php:39
+    'This news story exists in <strong>{{context_count}} contexts</strong>.' => 'このニュース記事は<strong>{{context_count}}つのコンテキスト</strong>にあります。',
+
+    #: config/common/post.config.php:40
+    'This news story exists in <strong>{{language_count}} languages</strong>.' => 'このニュース記事は<strong>{{language_count}}つの言語</strong>に翻訳されています。',
+
+    #: config/common/category.config.php:15
+    'Add a category' => 'カテゴリーを追加',
+
+);
