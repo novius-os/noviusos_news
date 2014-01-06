d3b94493f0c3bc6009d108f53688d4150638378c
Gilles FELIX <felix@novius.com>
2014-01-06 15:11:28 +0100

    "Add spanish translation"

---
+++ lang/es/common.lang.php
@@ -0,0 +1,88 @@
+<?php
+
+// Generated on 06/01/2014 15:07:24
+
+// 19 out of 19 messages are translated (100%).
+// 93 out of 93 words are translated (100%).
+
+return array(
+    #: classes/model/post.model.php:14
+    'Use news story title' => 'Utilizar el título de la noticia',
+
+    #: classes/model/post.model.php:15
+    'Use news story summary' => 'Utilizar el encabezado de la noticia',
+
+    #: config/controller/admin/appdesk.config.php:15
+    #: config/controller/admin/appdesk.config.php:21
+    'news stories' => 'noticias',
+
+    #: config/controller/admin/appdesk.config.php:16
+    #: config/controller/admin/appdesk.config.php:20
+    'news story' => 'noticia',
+
+    #: config/controller/admin/appdesk.config.php:23
+    #: config/common/post.config.php:36
+    '1 news story' => array(
+        0 => '1 noticia',
+        1 => '{{count}} noticias',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:27
+    'Showing 1 news story out of {{y}}' => array(
+        0 => 'Se muestra 1 noticia de las {{y}}',
+        1 => 'Se muestran {{x}} noticias de las {{y}}',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:30
+    'No news stories' => 'No hay noticias',
+
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:32
+    'Show all news stories' => 'Mostrar todas las noticias',
+
+    #: config/config.php:12
+    'News stories' => 'Noticias',
+
+    #: config/common/post.config.php:15
+    'Add a news story' => 'Añadir una noticia',
+
+    #. Crud
+    #: config/common/post.config.php:19
+    'Good! A new story added to your collection.' => '¡Estupendo! Has añadido una noticia a tu colección.',
+
+    #: config/common/post.config.php:20
+    'The news story has been deleted.' => 'Se ha eliminado la noticia.',
+
+    #. General errors
+    #: config/common/post.config.php:23
+    'This news story doesn’t exist any more. It has been deleted.' => 'Esta noticia ya no existe. Se ha eliminado.',
+
+    #: config/common/post.config.php:24
+    'We cannot find this news story.' => 'No encontramos esta noticia.',
+
+    #. Deletion popup
+    #: config/common/post.config.php:27
+    'Deleting the news story ‘{{title}}’' => 'Eliminar la noticia «{{title}}»',
+
+    #: config/common/post.config.php:31
+    'Yes, delete this news story' => array(
+        0 => 'Sí que quiero eliminar esta noticia',
+        1 => 'Sí que quiero eliminar estas {{count}} noticias',
+    ),
+
+    #: config/common/post.config.php:42
+    'This news story exists in <strong>one context</strong>.' => array(
+        0 => 'Esta noticia existe en <strong>un contexto</strong>.',
+        1 => 'Esta noticia existe en <strong>{{context_count}} contextos</strong>.',
+    ),
+
+    #: config/common/post.config.php:46
+    'This news story exists in <strong>one language</strong>.' => array(
+        0 => 'Esta noticia existe en <strong>un idioma</strong>.',
+        1 => 'Esta noticia existe en <strong>{{language_count}} idiomas</strong>.',
+    ),
+
+    #: config/common/category.config.php:14
+    'Add a category' => 'Añadir una categoría',
+
+);
---
+++ lang/es/metadata.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+// Generated on 06/01/2014 15:07:24
+
+// 2 out of 2 messages are translated (100%).
+// 6 out of 6 words are translated (100%).
+
+return array(
+    'News stories' => 'Noticias',
+
+    'Home page news stories' => 'Noticias (página de inicio)',
+
+);
