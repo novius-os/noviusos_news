e400912fa3cd55e40ff14da2929f38b483a929d3
foine <brousse@novius.fr>
2014-07-30 10:07:32 +0200

    "Japanese translation"

--- lang/ja/common.lang.php
+++ lang/ja/common.lang.php
@@ -1,17 +1,11 @@
 <?php
 
-// Generated on 11/12/2013 17:57:29
+// Generated on 30/07/2014 09:57:01
 
-// 19 out of 19 messages are translated (100%).
-// 93 out of 93 words are translated (100%).
+// 33 out of 33 messages are translated (100%).
+// 155 out of 155 words are translated (100%).
 
 return array(
-    #: classes/model/post.model.php:14
-    'Use news story title' => 'ニュース記事のタイトルを使用する',
-
-    #: classes/model/post.model.php:15
-    'Use news story summary' => 'ニュース記事の概要を使用する',
-
     #: config/controller/admin/appdesk.config.php:15
     #: config/controller/admin/appdesk.config.php:21
     'news stories' => 'ニュース記事',
@@ -38,8 +32,8 @@ return array(
     #: config/controller/admin/appdesk.config.php:32
     'Show all news stories' => 'ニュース記事を全て表示する',
 
-    #: config/config.php:12
-    'News stories' => 'ニュース記事',
+    #: config/common/category.config.php:14
+    'Add a category' => 'カテゴリーを追加',
 
     #: config/common/post.config.php:15
     'Add a news story' => 'ニュース記事を追加',
@@ -77,7 +71,55 @@ return array(
         0 => 'このニュース記事は<strong>{{language_count}}つの言語</strong>に翻訳されています。',
     ),
 
-    #: config/common/category.config.php:14
-    'Add a category' => 'カテゴリーを追加',
+    #: config/menu/item/category.config.php:14
+    'Add a news story category link' => 'ニュースカテゴリーリンクを追加',
+
+    #: config/menu/item/category.config.php:15
+    'New news story category link' => '新しいニュースカテゴリーリンク',
+
+    #: config/menu/item/category.config.php:46
+    'News story category:' => 'ニュースカテゴリー: ',
+
+    #: config/menu/item/category.config.php:57
+    'No news story category selected' => 'ニュースカテゴリーが選択されていません',
+
+    #: config/menu/item/category.config.php:58
+    'Pick a news story category' => 'ニュースカテゴリーを選択',
+
+    #: config/menu/item/category.config.php:59
+    'Pick another news story category' => '別のニュースカテゴリーを選択',
+
+    #: config/menu/item/category.config.php:60
+    'Un-select this news story category' => 'ニュースカテゴリーを外す',
+
+    #: config/menu/item/post.config.php:14
+    'Add a news story link' => 'ニュースリンクを追加',
+
+    #: config/menu/item/post.config.php:15
+    'New news story link' => '新しいニュースリンク',
+
+    #: config/menu/item/post.config.php:46
+    'News story post:' => 'ニュース記事: ',
+
+    #: config/menu/item/post.config.php:57
+    'No news story selected' => 'ニュース記事が選択されていません',
+
+    #: config/menu/item/post.config.php:58
+    'Pick a news story' => 'ニュース記事を選択',
+
+    #: config/menu/item/post.config.php:59
+    'Pick another news story' => '別のニュース記事を選択',
+
+    #: config/menu/item/post.config.php:60
+    'Un-select this news story' => 'ニュース記事を外す',
+
+    #: config/config.php:12
+    'News stories' => 'ニュース記事',
+
+    #: classes/model/post.model.php:16
+    'Use news story title' => 'ニュース記事のタイトルを使用する',
+
+    #: classes/model/post.model.php:17
+    'Use news story summary' => 'ニュース記事の概要を使用する',
 
 );
